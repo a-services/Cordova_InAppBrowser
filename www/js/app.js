1179b51c23c5fcd640d004219e1688f422cc2d7b
Egor Abramovich <egor.abramovich@gmail.com>
2022-02-25 23:47:36 +0300

    settings

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,3 @@
-// ------------- SETTINGS
-
-var loginPage = 'https://test-inappbrowser.app.appery.io/app/Login.html';
-var pages = [
-    loginPage,
-    'https://test-inappbrowser.app.appery.io/app/Screen1.html',
-    'https://test-inappbrowser.app.appery.io/app/Screen2.html'
-];
-
-var usernameInputID = 'Login_username_input';
-var passwordInputID = 'Login_password_input';
-var loginButtonName   = 'login_button';
-
-var siteUsername = 'test';
-var sitePassword = '123';
-
-// ------------- CODE
 
 document.addEventListener('deviceready', onDeviceReady, false);
 document.addEventListener('DOMContentLoaded', onDocumentReady, false);
